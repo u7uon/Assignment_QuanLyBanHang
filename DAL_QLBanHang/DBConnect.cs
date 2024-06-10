6c4835fa7e3c27fb99ef4cc3f5bde81755e25deb
eJwtzLFugzAQgOF3udkDlASM5y5RoqiqSqNOlrEP6mI46wxNaZR3rxqx/tL/3cBYiym90YATKHiqnZS4L2S5r6s6r4q8LKrMml1nSlm3+a6zLpOtBAGR6ds75G29bZKeN6r/JO26S0nPcXyn88epOV9Mf3QvzXAqDoffZc5em/Xrn3osel4jgoIWDSODgGTpEYwb/aTi0gZv9YCrYDROEfeCMZJYErLC0fggrsRDF+gKAvAnesakzQxqWkK43/8AHWhQDw== <duongnvpd10563@gmail.com>
2024-06-10 10:25:04 +0700

    final1

---
+++ DAL_QLBanHang/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QLBanHang
+{
+    public class DBConnect
+    {
+        //Tạo chuỗi kết nối đến cơ sở dữ liệu
+        protected SqlConnection _conn = new SqlConnection(@"Data Source=U7;Initial Catalog=QuanLyBanHangg;Integrated Security=True;Encrypt=False    ");
+
+    }
+}
